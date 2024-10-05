66c6a08e6e8a3f04bb594ce7be718ee387096fe9
viktorvidual <viktor.petrov@vidual.io>
2024-10-05 14:53:10 +0300

    Add Google Maps integration for game location

--- sportistbg/app/games/[id]/page.tsx
+++ sportistbg/app/games/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { fetchEvent } from "@/app/actions";
 import { notFound } from "next/navigation";
+import styles from "./page.module.css";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -13,9 +14,33 @@ export default async function Page({ params }: { params: { id: string } }) {
   const [event] = data;
 
   return (
-    <div>
+    <div className="flex gap-6 flex-col w-full p-3">
       <p className="text-2xl">Game Details: {event.name}</p>
       <p>{event.location}</p>
+      {/* Google Maps For Mobile */}
+      <div className="block md:hidden">
+        <iframe
+          width="100%"
+          height="350"
+          loading="lazy"
+          src={
+            "https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ7SxO9XJUpEARZg68Is2xLaM&key=" +
+            process.env.GOOGLE_MAPS_API_KEY
+          }
+        />
+      </div>
+      {/* Google Maps For Desktop */}
+      <div className="hidden md:block">
+        <iframe
+          width="600"
+          height="450"
+          loading="lazy"
+          src={
+            "https://www.google.com/maps/embed/v1/place?q=place_id:ChIJ7SxO9XJUpEARZg68Is2xLaM&key=" +
+            process.env.GOOGLE_MAPS_API_KEY
+          }
+        />
+      </div>
     </div>
   );
 }
